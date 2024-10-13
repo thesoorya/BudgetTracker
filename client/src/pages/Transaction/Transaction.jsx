f149d328c66f7a651c4e537d552e5e06999ad1a1
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-10-13 19:57:24 +0530

    transaction page completed

--- client/src/pages/Transaction/Transaction.jsx
+++ client/src/pages/Transaction/Transaction.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { StoreContext } from "../../context/Store";
+import { MdOutlineDelete } from "react-icons/md";
+import "./Transaction.css";
 
 const Transaction = () => {
-  return (
-    <div>Transaction</div>
-  )
-}
+  const { records, deleteRecord } = useContext(StoreContext);
+  const { user } = useUser();
 
-export default Transaction
+  return (
+    <div className="transaction-container">
+      <h4 className="transaction-title">Transaction Records</h4>
+      <ul className="transaction-list">
+        {records && records.length > 0 ? (
+          [...records].reverse().map((record) => (
+            <li className="transaction-item" key={record._id}>
+              <div
+                className="transaction-delete"
+                onClick={() => deleteRecord(record._id)}
+              >
+                <MdOutlineDelete />
+              </div>
+              <div className="transaction-amount">
+                <p
+                  className={`t-amount ${
+                    record.isCreditedOrDebited === "credited"
+                      ? "credited"
+                      : "debited"
+                  }`}
+                >
+                  <small>₹{record.amount}</small>
+                </p>
+                <p>
+                  <small>{record.isCreditedOrDebited}</small>
+                </p>
+                <div className="transaction-dates">
+                  <small>{new Date(record.date).toLocaleDateString()}</small>
+                  {" , "}
+                  <small>{new Date(record.date).toLocaleTimeString()}</small>
+                </div>
+              </div>
+
+              <div className="transaction-details">
+                <small>{record.category}</small>{" "}
+                <small>({record.paymentMethod})</small>
+              </div>
+            </li>
+          ))
+        ) : user ? (
+          <p className="no-records">No transaction records found.</p>
+        ) : (
+          <p className="no-records">
+            Please log in or sign up to view your transaction records.
+          </p>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Transaction;
